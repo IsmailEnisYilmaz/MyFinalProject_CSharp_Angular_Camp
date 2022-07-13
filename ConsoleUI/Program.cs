fce3e87483b01dfb3690d0737f24aa37abe9230f
Ismail Enis Yilmaz <yilmaz.ismailenis@gmail.com>
2022-07-14 00:04:11 +0300

    C#_Angular_Kamp_9.GunSonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -7,17 +7,33 @@ namespace ConsoleUI
 {
     //SOLID
     //Open Closed Principle -- yeni özellik eklerken hiçbir koduna dokunma
+    //DTO -- Data Transformation Object -- taşıyacağım objeler
     internal class Program
     {
         static void Main(string[] args)
         {
+            ProductTest();
+            //CategoryTest();
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetByUnitPrice(40,100))
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName + " / " + product.CategoryName);
             }
-
         }
     }
 }
--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -1,5 +1,6 @@
 ﻿using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -55,6 +56,11 @@ namespace DataAccess.Concrete.InMemory
         {
             //şarta uyanları yeni bir liste yapıp döndürür.
             return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            throw new NotImplementedException();
         }
 
         public void Update(Product product)
